b53b02933f3824f12123f666fc17c0a61452cf80
bluecrayon52 <16687465+bluecrayon52@users.noreply.github.com>
2019-01-06 11:31:48 -0500

    first commit

---
+++ src/View.js
@@ -0,0 +1,42 @@
+import hh from 'hyperscript-helpers';
+import { h } from 'virtual-dom'; 
+
+const { pre, div, h1, button, form, label, input} = hh(h); 
+
+function fieldSet(labelText, inputValue) {
+    return div([
+        label({ className: 'db mb1' }, labelText),
+        input({
+            className: 'pa2 input-reset ba w-100 mb2',
+            type: 'text',
+            value: inputValue, 
+        }),
+    ]);
+}
+
+function formView(dispatch, model) {
+    const { description, calories } = model; 
+    return form(
+        {
+            className: 'w-100 mv2',
+        },
+        [
+            fieldSet('Meal', description), 
+            fieldSet('Calories', calories || ''), 
+        ],
+    );
+    // return button(
+    //     {className: 'f3 pv2 ph3 bg-blue white bn'}, 
+    //     'Add meal',
+    // );
+
+}
+function view(dispatch, model) {
+    return div({className: 'mw6 center'}, [
+        h1({className: 'f2 pv2 bb'}, 'Calorie Counter'),
+        formView(dispatch, model),
+        pre(JSON.stringify(model, null, 2)),
+    ]); 
+}
+
+export default view; 
